e9ffb8e19221908e01d4ef344f7af5fbabd90865
MattCza <mtuczajkowski@gmail.com>
2023-04-22 20:14:48 +0200

    Added Response Status NOT_FOUND

--- src/main/java/com/example/EmployeeAPI/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/EmployeeAPI/exception/ResourceNotFoundException.java
@@ -1,7 +1,11 @@
 package com.example.EmployeeAPI.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.io.Serial;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
     @Serial
     private static final long serialVersionUID = 1L;
